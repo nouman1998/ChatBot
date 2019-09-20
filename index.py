77d0498703b88496fd30e42408a233fbd0e1f60e
Nouman Ejaz <46034570+nouman1998@users.noreply.github.com>
2019-09-20 23:12:48 +0500

    Add files via upload
    I have used Flask and Dialog Flow to compile and run I would prefer you to create a virtual environment as I have attached the dependencies file(requirments.txt).Use the following steps
    
    
    1- set the directory of CMD to root folder
    2- python -m venv abc (creating virtual env)
    3- abc/Scripts/activate
    4- pip install -r requirements.txt
    5- Flask run
    6- It would be running on http://127.0.0.1:5000/

---
+++ index.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify, render_template
+import os
+import dialogflow
+import requests
+import json
+import pusher
+
+app = Flask(__name__)
+
+# initialize Pusher
+pusher_client = pusher.Pusher(
+    app_id=os.getenv('PUSHER_APP_ID'),
+    key=os.getenv('PUSHER_KEY'),
+    secret=os.getenv('PUSHER_SECRET'),
+    cluster=os.getenv('PUSHER_CLUSTER'),
+    ssl=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    
+    if text:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+        
+        return response.query_result.fulfillment_text
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    try:
+        socketId = request.form['socketId']
+    except KeyError:
+        socketId = ''
+        
+    message = request.form['message']
+    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+    fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
+    response_text = { "message":  fulfillment_text }
+
+    pusher_client.trigger(
+        'movie_bot', 
+        'new_message', 
+        {
+            'human_message': message, 
+            'bot_message': fulfillment_text,
+        },
+        socketId
+    )
+                        
+    return jsonify(response_text)
+
+# run Flask app
+if __name__ == "__main__":
+    app.run()
---
+++ readme.txt
@@ -0,0 +1,9 @@
+I have used Flask and Dialog Flow to compile and run I would prefer you to create a virtual environment as I have attached the dependencies file(requirments.txt).Use the following steps
+
+
+1- set the directory of CMD to root folder
+2- python -m venv abc (creating virtual env)
+3- abc/Scripts/activate
+4- pip install -r requirements.txt
+5- Flask run
+6- It would be running on http://127.0.0.1:5000/
